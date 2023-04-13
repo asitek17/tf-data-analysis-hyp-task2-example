3aa7aca7e6f147f1795f37371510179f11ed787f
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 01:37:31 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
-import pandas as pd
-import numpy as np
-
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+import pandas as pd 
+import numpy as np 
+from scipy.stats import kruskal 
+ 
+chat_id = 694905952 # Ваш chat ID, не меняйте название переменной 
+ 
+def solution(x: np.array, y: np.array) -> bool: 
+    alpha = 0.07 
+    pvalue = kruskal(x, y).pvalue 
+     
+    return pvalue < alpha
